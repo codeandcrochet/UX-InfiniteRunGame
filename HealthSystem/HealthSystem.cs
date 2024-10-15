c46c91d720ec0427d8e416076077064be74b1dca
TylerD97 <120416173+TylerDawkins@users.noreply.github.com>
2024-10-15 15:53:11 -0400

    Created health system code

---
+++ HealthSystem/HealthSystem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using unityEngine;
+
+
+public class HealthSystem : MonoBehaviour
+{
+    public GameObject() heart;
+    public int life;
+
+    void Update()
+    {
+        if (life < 1)
+        {
+            Shatter(heart[0].gameObject);
+        }
+        else if (life < 2)
+        {
+            Shatter(heart[1].gameObject);
+        }
+        else if (life < 3)
+        {
+            Shatter(heart[3].gameObject);
+        }
+    }
+
+    public void CriticalHit(int z)
+    {
+        life -= z
+
+    }
+}
